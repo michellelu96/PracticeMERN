9c9f2b0174c7dd19bbeb02e0210d7752764781e7
michellelu96 <lu.michelle96@icloud.com>
2023-08-21 18:48:00 -0700

    basic frontend

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get('/api/products');
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
